467b31d51c99d02c4a85e3e3c6da405e0dcdc6ad
daniela ramirez alvarez <dani.maramirez@gmail.com>
2023-05-28 20:23:02 -0500

    Se creo una instancia de express-router para crear, eliminar y actualizar una tarea.

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const listViewRouter = require("./list-view-router");
+const listEditRouter = require("./list-edit-router");
 const listaTareas = require("./listaTareas.json");
 
 app.use(express.json());
 app.use("/tareas", listViewRouter);
+app.use("/tarea", listEditRouter);
 
 app.get("/", (req, res) => {
   res.send({
